2208717074a0bf2366247691503bad4847dbff14
Harry Foster <66202946+HarryFoster1812@users.noreply.github.com>
2024-11-11 04:33:31 +0400

    Finally got the gif to display something need to make it animated and finish the main menu

--- src/animate.py
+++ src/animate.py
@@ -1,19 +1,11 @@
 from tkinter import *
 from PIL import Image, ImageTk
-import time
 
-class App():
-
-    def __init__(self, root) -> None:
-        self.mainCanvas = Canvas(root, bg="#000", highlightthickness=0)
-        self.mainCanvas.pack(fill="both", expand="true")
-        self.mainMenu()
-        pass
-
-    def mainMenu(self):
-        file = "assets/title/title.gif"
+class Animate:
+    
+    def __init__(self, fileLoc, parent) -> None:
         # Open the image with PIL and convert to RGBA to preserve transparency
-        info = Image.open(file)
+        info = Image.open(fileLoc)
         framesNo = info.n_frames
         # Extract frames and keep references
         self.photoimage_objects = [] # i spent so long just to find out that this needs to be a local attribute since python was marking it for garbage collection so no image was displaying
@@ -23,17 +15,7 @@ class App():
             # Convert to ImageTk.PhotoImage to preserve transparency
             obj = ImageTk.PhotoImage(frame)
             self.photoimage_objects.append(obj)
+        
 
-        imageTitle = Label(self.mainCanvas, image=self.photoimage_objects[0], bg="#000")
-        imageTitle.pack()
+    def __del__() -> None:
         pass
-
-    def gameScreen():
-        pass
-
-if __name__ == "__main__":
-    root = Tk()
-    root.geometry("960x540")
-    root.title = "Woka Woka"
-    app = App(root)
-    root.mainloop()
