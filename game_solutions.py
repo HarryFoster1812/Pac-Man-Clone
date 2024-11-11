c6e5be8c135dfa304bc0ce68540c3572afffe3ca
Harry William Foster <harry.foster-2@student.manchester.ac.uk>
2024-11-11 14:48:04 +0000

    finished animating the gifs need to create the main menu and the leaderboard then need to create the game (maybe do boss screen before since it is easy)

--- game_solutions.py
+++ game_solutions.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from PIL import Image, ImageTk
-import time
+from src.animate import Animate
 
 class App():
 
@@ -8,28 +8,49 @@ class App():
         self.mainCanvas = Canvas(root, bg="#000", highlightthickness=0)
         self.mainCanvas.pack(fill="both", expand="true")
         self.mainMenu()
+
+        self.cleanUp = [] # add a list of attributes that need to be collected when switching to another screen
         pass
 
     def mainMenu(self):
-        file = "assets/title/title.gif"
-        # Open the image with PIL and convert to RGBA to preserve transparency
-        info = Image.open(file)
-        framesNo = info.n_frames
-        # Extract frames and keep references
-        self.photoimage_objects = [] # i spent so long just to find out that this needs to be a local attribute since python was marking it for garbage collection so no image was displaying
-        for i in range(framesNo):
-            info.seek(i)  # Move to the i-th frame
-            frame = info.copy().convert("RGBA")
-            # Convert to ImageTk.PhotoImage to preserve transparency
-            obj = ImageTk.PhotoImage(frame)
-            self.photoimage_objects.append(obj)
 
-        imageTitle = Label(self.mainCanvas, image=self.photoimage_objects[0], bg="#000")
-        imageTitle.pack()
+        titleLabel = Label(self.mainCanvas, image="", bg="#000")
+        titleLabel.pack()
+
+        forLegalLabel = Label(self.mainCanvas, image="", bg="#000")
+        forLegalLabel.pack()
+
+        self.title = Animate("assets/title/title.gif", titleLabel)
+        self.forLegal = Animate("assets/title/forLegal.gif", forLegalLabel)
+
+        # add current arrow
+
+        # play button
+
+        # options
+
+        # leaderboard
+
+        # exit
+
         pass
+
+    def nameScreen(self):
+
+        # add current highest score 
+
+        # add input box for the user name
+
+        # add validation
+
+        # add submit button
+
+        # switch to game screen
+
+        # back button at the top
 
     def gameScreen():
-        pass
+        raise NotImplementedError()
 
 if __name__ == "__main__":
     root = Tk()
--- src/animate.py
+++ src/animate.py
@@ -1,21 +1,52 @@
 from tkinter import *
 from PIL import Image, ImageTk
+import threading
 
 class Animate:
     
-    def __init__(self, fileLoc, parent) -> None:
+    def __init__(self, fileLoc: str, parent: Label | Canvas, x=None, y=None) -> None:
+        
+        self.fames = Animate.getFrames(fileLoc)
+        self.parent = parent
+        self.currentFrame = 0
+        self.delayMS = 1000//len(self.fames)
+        # add the image to the parent
+
+        if(type(self.parent) == Label):
+            self.parent.configure(image=self.fames[self.currentFrame])
+
+        else:
+            self.parent.create_image(x, y, image=self.fames[self.currentFrame], anchor="ne")
+        
+        self.loop =  self.parent.after(self.delayMS, self.update)
+        pass
+
+    def update(self):
+        self.currentFrame += 1 # increment the frame
+        self.currentFrame %= len(self.fames) # prevent a index error
+        
+        if(type(self.parent) == Label): 
+            self.parent.configure(image=self.fames[self.currentFrame])
+
+        self.loop = self.parent.after(self.delayMS, self.update) # recall the loop
+        
+        
+
+    def getFrames(fileLoc) -> list:
         # Open the image with PIL and convert to RGBA to preserve transparency
         info = Image.open(fileLoc)
         framesNo = info.n_frames
         # Extract frames and keep references
-        self.photoimage_objects = [] # i spent so long just to find out that this needs to be a local attribute since python was marking it for garbage collection so no image was displaying
+        frames = [] # i spent so long just to find out that this needs to be a local attribute since python was marking it for garbage collection so no image was displaying
         for i in range(framesNo):
             info.seek(i)  # Move to the i-th frame
             frame = info.copy().convert("RGBA")
             # Convert to ImageTk.PhotoImage to preserve transparency
             obj = ImageTk.PhotoImage(frame)
-            self.photoimage_objects.append(obj)
+            frames.append(obj)
+
+        return frames
         
 
-    def __del__() -> None:
-        pass
+    def __del__(self) -> None:
+        self.parent.after_cancel(self.loop)
