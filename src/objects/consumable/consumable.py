eb17efde3119c7a8e829b8917f7e3a99b43f4020
r49769hf <harry.foster-2@sudent.manchester.ac.uk>
2024-11-21 18:50:05 +0000

    removed consumables since i didnt implement them so there is no need for the classes

--- src/objects/consumable/consumable.py
+++
@@ -1,8 +0,0 @@
-class consumable():
-    
-    def __init__(self) -> None:
-        self.points = 0
-        self.imageSrc = ""
-        self.location = ""
-        self.isEnabled = True
-        pass
